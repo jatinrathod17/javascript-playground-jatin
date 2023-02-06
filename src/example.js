545cb39990edf29b74bba209a25029357a054731
jatinrathod17 <jatinrathod17@gmail.com>
2023-02-06 17:59:54 +0530

    Initial Changes.

--- src/example.js
+++ src/example.js
@@ -5,36 +5,36 @@ const abc = () => {
 //console.log(i);
 
 const multiSum = (a, b) => a + b;
-const currySum = (a) => (b) => a + b;
+const currySum = a => b => a + b;
 
 //console.log("Multi and Curry Sum", multiSum(2, 3), currySum(2)(3));
 
 // Add Numbers
 //A=[1,2,3,4,5,6,7,8,9,10]
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const arraySum = (numbers) => {
+const arraySum = numbers => {
   return numbers.reduce((acc, value) => {
     debugger;
     return acc + value;
   }, 0);
 };
-console.log("Array sum with sReduce Method", arraySum(array));
+//console.log("Array sum with sReduce Method", arraySum(array));
 
 // Flat Array
 const mainArray = [1, 2, 3, 4, [4, 5, 6, [8, 9, 10]]];
 //const flatArray = [].concat(...mainArray);
-const flatArray = mainArray.flat(2);
-const flatMapArray = mainArray.flatMap((num) => num);
+const flatArray = mainArray.flat(Infinity);
+const flatMapArray = mainArray.flatMap(num => num);
 const objArray = [
   { userId: 1, username: "Tom", attributes: ["Nice", "Cute"] },
   { userId: 2, username: "Mike", attributes: ["Lovely"] }
-].flatMap((x) => x.attributes);
+].flatMap(x => x.attributes);
 // Output => ["Nice", "Cute", "Lovely"]
 
 //console.log("Flat and FlatMap Array", flatArray, flatMapArray, objArray);
 
-const student = (name) => {
-  const yourGrade = (percentage) => {
+const student = name => {
+  const yourGrade = percentage => {
     if (percentage > 35) {
       console.log(`Great ${name} You are Pass`);
     } else {
@@ -50,12 +50,12 @@ const studentInfo1 = student("Nikunj");
 //studentInfo1(32);
 
 const promise = new Promise(
-  (resolve) => {
+  resolve => {
     setTimeout(() => {
       resolve("Promise Resolved");
     }, 2000);
   },
-  (reject) => {}
+  reject => {}
 );
 
 //promise.then((value) => console.log(value));
@@ -65,10 +65,10 @@ var age = 12;
 const userObj = {
   age: 15,
   displayAge() {
-    //console.log("Display Age", this.age);
+    console.log("Display Age", this.age);
   },
   showAge: () => {
-    //console.log("Show Age 1", age);
+    console.log("Show Age 1", age);
   }
 };
 
@@ -86,7 +86,6 @@ const sum = (nums, target) => {
 
   for (i = 0; i < nums.length; i++) {
     let difference = target - nums[i];
-    console.log(difference, indexMap);
     if (indexMap.has(difference)) {
       result[0] = indexMap.get(difference);
       result[1] = i;
@@ -100,3 +99,17 @@ const sum = (nums, target) => {
 };
 
 // console.log(sum([2, 4, 1, 15, 7], 9));
+
+const sumOfUnequalArray = (array1, array2) => {
+  let a, b;
+  if (array1.length > array2.length) {
+    a = array1;
+    b = array2;
+  } else {
+    a = array2;
+    b = array1;
+  }
+  return a.map((num, idx) => num + (b[idx] || 0));
+};
+
+console.log(sumOfUnequalArray([1, 2, 3, 4], [5, 6, 7, 8, 100]));
--- src/index.js
+++ src/index.js
@@ -8,5 +8,4 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
-
 //[20, 10, 5, 2, 1, 0.5, 0.25, 0.10, 0.05, 0.01]
